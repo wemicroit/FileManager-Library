edbda1cb715b42a559c2f86035e8314d76641e70
James Thompson <wemicroit@outlook.com>
2018-11-28 00:02:10 +1100

    Got the transform all implemented.

---
+++ FileManager/Transform.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using WeMicroIt.Utils.FileConverter.Interfaces;
+
+namespace WeMicroIt.Utils.FileConverter
+{
+    public partial class FileManager : IFileManager
+    {
+        public bool TransformXML(bool persist = false)
+        {
+            string end = TransformerInfo.FullPath;
+            if (persist)
+            {
+                end = WriterInfo.FullPath;
+            }
+            if(!XMLConverter.TransformObjects(ReaderInfo.FullPath, TemplateInfo.FullPath, end))
+            {
+                return false;
+            }
+            ReaderInfo.FullPath = TransformerInfo.FullPath;
+            return true;
+        }
+    }
+}
