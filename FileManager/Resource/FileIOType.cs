b4e47f647792b647baa5560ef534cdb1f4e6a999
James Thompson <wemicroit@outlook.com>
2018-11-28 23:57:52 +1100

    Added an enum to designate what type of io is to occur.

---
+++ FileManager/Resource/FileIOType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeMicroIt.Utils.FileConverter.Resource
+{
+    public enum FileIOType
+    {
+        Block = 0,
+        Lines = 1,
+        Line = 2,
+        Data = 3,
+        Stream = 4
+    }
+}
